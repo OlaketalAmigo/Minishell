f9c864060c5e9dc1a8dc98b1d3ded4d295007398
tfauve-p <thomasfauvepiot@gmail.com>
2024-08-06 12:12:10 +0200

    start

---
+++ INCLUDES/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfauve-p <tfauve-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/25 11:53:12 by tfauve-p          #+#    #+#             */
+/*   Updated: 2024/08/06 11:55:50 by tfauve-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+# include "stdio.h"
+# include "stdlib.h"
+# include "readline/readline.h"
+# include "readline/history.h"
+
+#endif
