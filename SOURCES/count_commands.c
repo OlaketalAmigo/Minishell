7ffc626636f13833340f426bb80527cb091449b0
gprunet <hehe@42Cursus.myguest.virtualbox.org>
2024-12-14 15:42:48 +0100

    wrong cmd good

--- SOURCES/count_commands.c
+++ SOURCES/count_commands.c
@@ -6,27 +6,11 @@
 /*   By: hehe <hehe@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 13:47:27 by hehe              #+#    #+#             */
-/*   Updated: 2024/12/14 15:36:42 by hehe             ###   ########.fr       */
+/*   Updated: 2024/12/14 15:42:43 by hehe             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	count_commands_utils(char **arg, t_struct *data, int i, int *count)
-{
-	if (ft_strchr(arg[i], '/') == 1)
-	{
-		if (ft_check_hard_path(data, arg[i]))
-			*count = *count + 1;
-	}
-	else if (is_empty(arg[i]) == 0)
-	{
-		if (ft_check_builtins_init(arg[i]) || ft_strchr(arg[i], '|'))
-			*count = *count + 1;
-		else if (ft_check_path(data, arg[i]))
-			*count = *count + 1;
-	}
-}
 
 int	count_commands(char **arg, t_struct *data)
 {
@@ -37,19 +21,7 @@ int	count_commands(char **arg, t_struct *data)
 	count = 0;
 	while (arg[i])
 	{
-		if (i == 0 || i)
-		{
-			count++;
-			i++;
-			continue ;
-		}
-		if (check_redirection_cmd(arg[i]) == 1)
-		{
-			count++;
-			i++;
-			continue ;
-		}
-		count_commands_utils(arg, data, i, &count);
+		count++;
 		i++;
 	}
 	return (count);
